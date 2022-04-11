4712905a24b1c0c059ab38f49da3b4354fdbdbe1
arif5248 <arifislam11ctg@gmail.com>
2022-04-12 04:31:09 +0600

    bug fixed for date and month

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ function digitalClock(){
   hours= hours<10 ? '0'+hours: hours;
   minutes=minutes<10 ? '0'+minutes : minutes;
   seconds= seconds<10 ? '0'+seconds : seconds;
-
+  date=date<10 ? '0'+date : date;
+  month= month<10 ? '0'+month : month
   let finalTime=`${hours}:${minutes}:${seconds}`;
   let finalDate = `${month}/${date}/ ${year}`;
   
