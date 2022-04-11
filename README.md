d1afe68517e9a3c191b0210774ba7fbe7c2b7519
arif5248 <arifislam11ctg@gmail.com>
2022-04-12 03:41:11 +0600

    my project is done

---
+++ README.md
@@ -0,0 +1,1 @@
+# live-date-and-time
---
+++ app.js
@@ -0,0 +1,42 @@
+function digitalClock(){
+  let allDate= new Date();
+  
+  let hours= allDate.getHours();
+  let minutes=allDate.getMinutes();
+  let seconds= allDate.getSeconds();
+  let date = allDate.getDate();
+  let month = allDate.getMonth();
+  let year = allDate.getFullYear();
+  let clockFormate='AM';
+
+  if(hours==0){
+    hours=12;
+  }
+  else if(hours>12){
+    hours=hours-12;
+    clockFormate='PM'
+  }
+
+  hours= hours<10 ? '0'+hours: hours;
+  minutes=minutes<10 ? '0'+minutes : minutes;
+  seconds= seconds<10 ? '0'+seconds : seconds;
+
+  let finalTime=`${hours}:${minutes}:${seconds}`;
+  let finalDate = `${month}/${date}/ ${year}
+    `;
+  let test= ` 
+  <ul>
+   <li> Time: ${finalTime} </li>
+  </ul> `
+  console.log(test);
+  document.querySelector('#time').innerText=finalTime;
+  document.querySelector('small').innerText=clockFormate;
+  document.querySelector('span').innerText=finalDate;
+  
+  setInterval(digitalClock, 1000);
+  
+}
+
+digitalClock();
+
+
